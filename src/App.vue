4ecaf6c7b21068cd2be3267756607c7d3207c6ae
Sam <samuv.eth@gmail.com>
2021-01-17 14:02:15 +0700

    Change global font

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
-  <div class="h-screen bg-gray-100 px-4 py-6">
+  <div class="min-h-screen bg-gray-100 px-4 pt-6 pb-20">
     <router-view />
   </div>
 </template>
 
-<script>
-</script>
+<script></script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Source+Code+Pro&display=swap');
+
+html,
+body {
+  font-family: 'Source Code Pro', monospace;
+}
 </style>
