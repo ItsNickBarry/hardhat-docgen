e96b705b9c98392463ddf51dcb5b0b17179ed2f4
Sam <samuv.eth@gmail.com>
2021-01-17 14:06:06 +0700

    Style and refactor

--- src/components/Member.vue
+++ src/components/Member.vue
@@ -1,46 +1,35 @@
 <template>
-  <div class="member w-full">
-    <h3>{{ name }} <code>{{ keywords }} {{ inputSignature }}</code></h3>
+  <div class="border-2 border-gray-400 border-dashed w-full p-2">
+    <h3 class="text-lg pb-2 mb-2 border-b-2 border-gray-400 border-dashed">
+      {{ name }} {{ keywords }} {{ inputSignature }}
+    </h3>
 
-    <p>{{ json.notice }}</p>
+    <div class="space-y-3">
+      <p>{{ json.notice }}</p>
 
-    <p>{{ json.details }}</p>
+      <p>{{ json.details }}</p>
 
-    <ul v-if="inputs.length > 0">
-      <h4>Parameters</h4>
-      <li
-        v-for="(input, index) in inputs"
-        :key="index"
-      >
-        <code>{{ input.type }}</code> <b>{{ input.name }}</b>: <i>{{ input.desc }}</i>
-      </li>
-    </ul>
-
-    <ul v-if="outputs.length > 0">
-      <h4>Return Values</h4>
-      <li
-        v-for="(output, index) in outputs"
-        :key="index"
-      >
-        <code>{{ output.type }}</code> <b>{{ output.name || `_${ index }` }}</b>: <i>{{ output.desc }}</i>
-      </li>
-    </ul>
+      <MemberSection :puts="inputs" name="Parameters" />
+      <MemberSection :puts="outputs" name="Return Values" />
+    </div>
   </div>
 </template>
 
 <script>
+import MemberSection from './MemberSection.vue';
 export default {
+  components: { MemberSection },
   props: {
-    json: { type: Object, default: () => new Object() },
+    json: { type: Object, default: () => new Object() }
   },
 
   computed: {
-    name: function () {
+    name: function() {
       // special functions are unnamed; default to type
       return this.json.name || this.json.type;
     },
 
-    keywords: function () {
+    keywords: function() {
       let keywords = [];
 
       if (this.json.stateMutability) {
@@ -54,40 +43,35 @@ export default {
       return keywords.join(' ');
     },
 
-    params: function () {
+    params: function() {
       return this.json.params || {};
     },
 
-    returns: function () {
+    returns: function() {
       return this.json.returns || {};
     },
 
-    inputs: function () {
-      return (this.json.inputs || []).map(i => ({ ...i, desc: this.params[i.name] }));
+    inputs: function() {
+      return (this.json.inputs || []).map(i => ({
+        ...i,
+        desc: this.params[i.name]
+      }));
     },
 
-    inputSignature: function () {
-      return `(${ this.inputs.map(i => i.type).join(',') })`;
+    inputSignature: function() {
+      return `(${this.inputs.map(i => i.type).join(',')})`;
     },
 
-    outputs: function () {
-      return (this.json.outputs || []).map((i, index) => ({ ...i, desc: this.returns[i.name || `_${ index }`] }));
+    outputs: function() {
+      return (this.json.outputs || []).map((i, index) => ({
+        ...i,
+        desc: this.returns[i.name || `_${index}`]
+      }));
     },
 
-    outputSignature: function () {
-      return `(${ this.outputs.map(i => i.type).join(',') })`;
-    },
-  },
+    outputSignature: function() {
+      return `(${this.outputs.map(i => i.type).join(',')})`;
+    }
+  }
 };
 </script>
-
-<style>
-code {
-  background: lightgray;
-}
-
-.member {
-  margin-top: 10px;
-  border: 1px dashed gray;
-}
-</style>
---
+++ src/components/MemberSection.vue
@@ -0,0 +1,16 @@
+<template>
+  <ul v-if="puts.length > 0">
+    <h4 class="text-lg">{{ name }}</h4>
+    <li v-for="(put, index) in puts" :key="index">
+      <span class="bg-gray-300">{{ put.type }}</span>
+      <b>{{ put.name || `_${index}` }}</b
+      >: <i>{{ put.desc }}</i>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['puts', 'name']
+};
+</script>
