e712172c8ee860bc451248d56ce256ee0838cd40
Nick Barry <itsnickbarry@protonmail.ch>
2021-01-16 20:00:08 -0600

    add Branch component for recursive source display

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,20 +1,41 @@
 <template>
   <div class="w-full space-y-10 md:max-w-screen-sm lg:max-w-screen-md m-auto">
-    <p
-      v-for="path in Object.keys(json)"
-      :key="path"
-    >
-      <router-link :to="path">
-        {{ path }}
-      </router-link>
-    </p>
+    <Branch
+      :json="trees"
+      name="Sources:"
+    />
   </div>
 </template>
 
 <script>
+import Branch from './Branch.vue';
+
 export default {
+  components: { Branch },
+
   props: {
     json: { type: Object, default: () => new Object() },
+  },
+
+  computed: {
+    trees: function () {
+      let trees = {};
+
+      for (let path in this.json) {
+        path.split('/').reduce(function (acc, dir) {
+          if (dir.includes(':')) {
+            let [file] = dir.split(':');
+            acc[file] = acc[file] || [];
+            acc[file].push(this.json[path]);
+          } else {
+            acc[dir] = acc[dir] || {};
+            return acc[dir];
+          }
+        }.bind(this), trees);
+      }
+
+      return trees;
+    },
   },
 };
 </script>
