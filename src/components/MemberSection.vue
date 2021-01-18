6f49f48ce099a66871ee33921492e7969cc0e499
Nick Barry <itsnickbarry@protonmail.ch>
2021-01-18 01:05:34 -0600

    remove trailing colons from items without descriptions

--- src/components/MemberSection.vue
+++ src/components/MemberSection.vue
@@ -1,16 +1,20 @@
 <template>
   <ul v-if="puts.length > 0">
-    <h4 class="text-lg">{{ name }}</h4>
-    <li v-for="(put, index) in puts" :key="index">
+    <h4 class="text-lg">
+      {{ name }}
+    </h4>
+    <li
+      v-for="(put, index) in puts"
+      :key="index"
+    >
       <span class="bg-gray-300">{{ put.type }}</span>
-      <b>{{ put.name || `_${index}` }}</b
-      >: <i>{{ put.desc }}</i>
+      <b>{{ put.name || `_${index}` }}</b><span v-if="put.desc">: <i>{{ put.desc }}</i></span>
     </li>
   </ul>
 </template>
 
 <script>
 export default {
-  props: ['puts', 'name']
+  props: ['puts', 'name'],
 };
 </script>
