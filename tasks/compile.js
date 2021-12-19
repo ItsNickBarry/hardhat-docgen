d5914b65155f71403eb14d9dcd8a12314fcec4b6
Nick Barry <itsnickbarry@protonmail.ch>
2021-12-19 01:56:04 -0600

    define tasks in tasks directory

---
+++ tasks/compile.js
@@ -0,0 +1,16 @@
+const {
+  TASK_COMPILE,
+} = require('hardhat/builtin-tasks/task-names');
+
+task(TASK_COMPILE, async function (args, hre, runSuper) {
+  for (let compiler of hre.config.solidity.compilers) {
+    compiler.settings.outputSelection['*']['*'].push('devdoc');
+    compiler.settings.outputSelection['*']['*'].push('userdoc');
+  }
+
+  await runSuper();
+
+  if (hre.config.docgen.runOnCompile) {
+    await hre.run('docgen');
+  }
+});
